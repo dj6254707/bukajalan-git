1ecbd236fb8b7249cec90b2d730bea87cd640f5c
Dave Johnson <47549398+dj6254707@users.noreply.github.com>
2019-05-11 01:33:03 +0700

    Bukajalan 2.0

---
+++ build/precache-manifest.170cb96190fe163ae263ad7af55a5c61.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a8b921bc7a86138ecd6",
+    "url": "/static/css/main.3b3b8269.chunk.css"
+  },
+  {
+    "revision": "0a8b921bc7a86138ecd6",
+    "url": "/static/js/main.0a8b921b.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "ce22a84fc8f4034d5a7a",
+    "url": "/static/js/2.ce22a84f.chunk.js"
+  },
+  {
+    "revision": "f4ad462575e303252789fe5e8bbe61bb",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.188e56d0cdd6899b69fa14fc2fe95d8f.js"
+  "/precache-manifest.170cb96190fe163ae263ad7af55a5c61.js"
 );
 
 workbox.clientsClaim();
